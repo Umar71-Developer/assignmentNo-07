51db694114e750eab6774fc71e4e79361ce041d6
Umar71-Developer <umarhayyat710000@gmail.com>
2023-09-09 02:00:15 +0500

    2 questions are added here

---
+++ assignmentNo-07.js
@@ -0,0 +1,14 @@
+"use strict";
+//Q2...Personal Message: Store a person's name in a variable, and print a message to that person. Your message should be simple, such as, "Hello Eric,would you like to learn some Python today?"
+var name1 = "Bahadar";
+console.log(name1);
+var Message = "Hello Bahadar, would you like to learn some Python today?";
+console.log(Message);
+console.log("**********************************************************");
+//Q3...Name Cases : Store a person's name in a variable, and then print that person's name in lowercase, uppercase, and titlecase,
+var name2 = "Bahadar Ali";
+console.log(name2);
+const lowercase = name2.toLowerCase();
+console.log(lowercase);
+const uppercase = name2.toUpperCase();
+console.log(uppercase);
---
+++ assignmentNo-07.ts
@@ -0,0 +1,15 @@
+//Q2...Personal Message: Store a person's name in a variable, and print a message to that person. Your message should be simple, such as, "Hello Eric,would you like to learn some Python today?"
+var name1:string = "Bahadar";
+console.log(name1);
+ var Message:string = "Hello Bahadar, would you like to learn some Python today?"
+ console.log(Message);
+ console.log("**********************************************************");
+ //Q3...Name Cases : Store a person's name in a variable, and then print that person's name in lowercase, uppercase, and titlecase,
+ var name2:string = "Bahadar Ali";
+ console.log(name2);
+ const lowercase =name2.toLowerCase();
+ console.log(lowercase);
+ const uppercase =  name2.toUpperCase();
+ console.log(uppercase);
+ 
+ 
